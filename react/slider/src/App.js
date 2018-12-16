8757654bb7c9b2d2123cd5bb1bb0000eef7bf96a
Oleksandr Shepelenko <sashashepelenko@gmail.com>
2018-12-16 11:02:33 +0200

    Add files via upload

--- react/slider/src/App.js
+++ react/slider/src/App.js
@@ -1,30 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg'
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className = "App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper> 
+        <MainWrapper>
+          <Grid>
+           <Main /> 
+          </Grid>
+        </MainWrapper>      
       </div>
     );
   }
